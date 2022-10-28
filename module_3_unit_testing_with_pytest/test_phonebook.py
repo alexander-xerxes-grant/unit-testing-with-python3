7e38e9c90c752a3adb28793b259aa0bd8f497ffc
Alexander Grant <alexander.grant@dunelm.com>
2022-10-28 12:08:04 +0100

    test missing name raises Key Error

--- module_3_unit_testing_with_pytest/test_phonebook.py
+++ module_3_unit_testing_with_pytest/test_phonebook.py
@@ -1,3 +1,5 @@
+import pytest
+
 from module_3_unit_testing_with_pytest.phonebook import Phonebook
 
 
@@ -13,3 +15,13 @@ def test_phonebook_contains_all_names():
     phonebook.add("Alex", "12345")
 
     assert "Alex" in phonebook.names()
+
+
+def test_missing_name_raises_error():
+    phonebook = Phonebook()
+    phonebook.add("Alex", "12345")
+
+    with pytest.raises(KeyError) as error:
+        phonebook.lookup("John")
+
+    assert error.type == KeyError
