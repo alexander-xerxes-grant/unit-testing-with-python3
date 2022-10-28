85b1c935674b8a6cd9cfd26dacdb0fc9124852b7
Alexander Grant <alexander.grant@dunelm.com>
2022-10-28 12:13:13 +0100

    create fixture for phonebook

--- module_3_unit_testing_with_pytest/test_phonebook.py
+++ module_3_unit_testing_with_pytest/test_phonebook.py
@@ -3,22 +3,25 @@ import pytest
 from module_3_unit_testing_with_pytest.phonebook import Phonebook
 
 
-def test_lookup_by_name():
+@pytest.fixture
+def phonebook():
     phonebook = Phonebook()
+    return phonebook
+
+
+def test_lookup_by_name(phonebook):
     phonebook.add("Alex", "12345")
 
     assert "12345" == phonebook.lookup("Alex")
 
 
-def test_phonebook_contains_all_names():
-    phonebook = Phonebook()
+def test_phonebook_contains_all_names(phonebook):
     phonebook.add("Alex", "12345")
 
     assert "Alex" in phonebook.names()
 
 
-def test_missing_name_raises_error():
-    phonebook = Phonebook()
+def test_missing_name_raises_error(phonebook):
     phonebook.add("Alex", "12345")
 
     with pytest.raises(KeyError):
