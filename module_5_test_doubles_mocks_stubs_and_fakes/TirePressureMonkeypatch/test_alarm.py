d3c6a866ae6613bee4e1dd52a19ef0d34e99b244
Alexander Grant <alexander.grant@dunelm.com>
2022-10-30 09:36:54 +0000

    test alarm turns on when high pressure threshold passed

--- module_5_test_doubles_mocks_stubs_and_fakes/TirePressureMonkeypatch/test_alarm.py
+++ module_5_test_doubles_mocks_stubs_and_fakes/TirePressureMonkeypatch/test_alarm.py
@@ -1,3 +1,17 @@
+from unittest.mock import Mock, patch
+
+from alarm import (
+    Alarm,
+)
 
 
+def test_alarm_with_high_pressure_value():
+    with patch("alarm.Sensor") as test_sensor_class:
+        test_sensor_instance = Mock()
+        test_sensor_instance.sample_pressure.return_value = 22
+        test_sensor_class.return_value = test_sensor_instance
 
+        alarm = Alarm()
+        alarm.check()
+
+        assert alarm.is_alarm_on
