af6d32544ef4f5a247ab5ce518b1f5264f3a6e9f
Alexander Grant <alexander.grant@dunelm.com>
2022-10-30 09:06:33 +0000

    add access second page test

--- module_5_test_doubles_mocks_stubs_and_fakes/HtmlConverter/test_html_pages.py
+++ module_5_test_doubles_mocks_stubs_and_fakes/HtmlConverter/test_html_pages.py
@@ -34,3 +34,18 @@ def test_convert_quotes():
     converter = HtmlPagesConverter(open_file=fake_file)
     converted_text = converter.get_html_page(0)
     assert converted_text == "quote: &#x27;<br />"
+
+
+def test_access_second_page():
+    fake_file = io.StringIO(
+        """\
+page one
+PAGE_BREAK
+page two
+PAGE_BREAK
+page three
+"""
+    )
+    converter = HtmlPagesConverter(open_file=fake_file)
+    converted_text = converter.get_html_page(1)
+    assert converted_text == "page two<br />"
