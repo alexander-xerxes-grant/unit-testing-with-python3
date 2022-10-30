83ef6bcb8d579e733009621740f51dac06d9b96d
Alexander Grant <alexander.grant@dunelm.com>
2022-10-30 09:41:40 +0000

    test alarm for low pressure threshold passed

--- module_5_test_doubles_mocks_stubs_and_fakes/TirePressureMonkeypatch/test_alarm.py
+++ module_5_test_doubles_mocks_stubs_and_fakes/TirePressureMonkeypatch/test_alarm.py
@@ -15,3 +15,15 @@ def test_alarm_with_high_pressure_value():
         alarm.check()
 
         assert alarm.is_alarm_on
+
+
+@patch("alarm.Sensor")
+def test_alarm_with_low_pressure(test_sensor_class):
+    test_sensor_instance = Mock()
+    test_sensor_instance.sample_pressure.return_value = 16
+    test_sensor_class.return_value = test_sensor_instance
+
+    alarm = Alarm()
+    alarm.check()
+
+    assert alarm.is_alarm_on
